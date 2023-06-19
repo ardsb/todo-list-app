8fcec6e73c4faeadd9b163b32945162a6832539b
Arkam <arkam.ardsb@gmail.com>
2023-06-19 19:47:37 +0530

    adding todo is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,58 @@
 
 import "./styles.css"
+import { useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
 
 function App() {
+
+
+  const [todos, setTodos] = useState([])
   
 
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
+  }
+  console.log(todos)
   return (
-    <> 
-<form className="new-item-form">
-<div className="form-row">
-<label htmlFor="item"> New Item</label>
-<input type="text" id="item"></input>
-</div>
-<button className="btn">
-Add
-
-</button>
-</form>
-<h1 className="header">
-  Todo List
-</h1>
-<ul className="list">
-  <li>
-  <label>
-    <input type="checkbox"/>
-
     
-  </label>
+    <>
+      <NewTodoForm onSubmit={addTodo}/>
+    
+      <h1 className="header">
+        Todo List
+      </h1>
+     
+      
+      <ul  className="list">
+      {todos.map(todo => {
+        // eslint-disable-next-line react/jsx-key
+        return (
+          <> 
+          <li key={todo.id}>
+          <label>
+            <input type="checkbox"  checked={todo.completed}/>
+            {todo.title}
 
-  </li>
-</ul>
-</>
-  
+          </label>
+          <button className="btn btn-danger">
+            delete
+          </button>
+
+        </li>
+        </>
+        )
+        
+      })}
+      
+      </ul>
+    </>
+
+
 
   )
 }
